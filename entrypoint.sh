32aa31ce371648887287210911501fc2ad15add0
Siavash Bamshadnia <sbamtr@gmail.com>
2025-07-20 09:16:50 +0330

    feat: add filament optimization commands to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,11 @@ php artisan migrate --force --seed --seeder=ProductionDatabaseSeeder
 
 # Run Laravel optimization commands
 php artisan optimize:clear
+php artisan filament:optimize-clear
 php artisan optimize
 php artisan view:cache
 php artisan event:cache
+php artisan filament:optimize
 
 # Start FrankenPHP server
 exec php artisan octane:start --server=frankenphp --workers=$OCTANE_WORKERS
