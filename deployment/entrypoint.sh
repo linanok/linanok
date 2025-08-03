a31a9cec0cf6b14c6958bd5eee94564b54e641c0
Siavash Bamshadnia <sbamtr@gmail.com>
2025-08-03 16:39:07 +0330

    feat: add init service to docker-compose and refactor entrypoint script

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -6,9 +6,5 @@ cd /app
 # Run database migrations
 php artisan migrate --force --seed --seeder=ProductionDatabaseSeeder
 
-# Run Laravel optimization commands
-php artisan optimize:clear
-php artisan optimize
-
 # Start FrankenPHP server
 exec php artisan octane:start --server=frankenphp --workers=$OCTANE_WORKERS
