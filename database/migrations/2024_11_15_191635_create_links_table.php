aecad3c693c775c5dc81495ad1c2837eafd7144b
Siavash Bamshadnia <sbamtr@gmail.com>
2025-08-05 12:23:06 +0330

    fix: set collation to utf8_general_ci only on mysql, mariadb

--- database/migrations/2024_11_15_191635_create_links_table.php
+++ database/migrations/2024_11_15_191635_create_links_table.php
@@ -10,8 +10,13 @@ return new class extends Migration
     {
         Schema::create('links', function (Blueprint $table) {
             $table->id();
-            $table->string('original_url', 2048)->charset('utf8')->index();
-            $table->string('short_path')->charset('utf8')->index();
+            if (config('database.default') === 'mysql' || config('database.default') === 'mariadb') {
+                $table->string('original_url', 2048)->charset('utf8')->collation('utf8_general_ci')->index();
+                $table->string('short_path')->charset('utf8')->collation('utf8_general_ci')->index();
+            } else {
+                $table->string('original_url', 2048)->index();
+                $table->string('short_path')->index();
+            }
             $table->string('password')->nullable();
             $table->boolean('is_active')->default(true);
             $table->dateTime('available_at')->nullable();
