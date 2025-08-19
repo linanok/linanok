a381a1a12f5086f07d58fbfb91ec75334eb7d7a6
Siavash Bamshadnia <sbamtr@gmail.com>
2025-08-19 09:35:27 +0330

    fix: style in copyright.blade.php

--- resources/views/components/copyright.blade.php
+++ resources/views/components/copyright.blade.php
@@ -1,5 +1,5 @@
 <footer
-    class="mt-auto w-full py-4 text-center text-sm text-gray-600 dark:text-gray-400 bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700">
+    class="mt-auto w-full !py-4 text-center text-sm text-gray-600 dark:text-gray-400 bg-white dark:bg-gray-800">
     <p>
         &copy; 2025 - {{ date('Y') }}
         <a href="https://linanok.com" target="_blank">{{ config('app.name', 'Laravel') }}</a>.
