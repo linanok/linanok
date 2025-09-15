4b6f0ce18cc0bd00c6ff7eca73a614f66b64ed80
Siavash Bamshadnia <sbamtr@gmail.com>
2025-09-15 16:49:06 +0330

    fix: prevent users from editing their own roles, permissions and restrict role, permission editing to super admins

--- app/Policies/RolePolicy.php
+++ app/Policies/RolePolicy.php
@@ -2,6 +2,9 @@
 
 namespace App\Policies;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+
 /**
  * Role Policy
  *
@@ -16,4 +19,13 @@ class RolePolicy extends BasePolicy
 {
     /** @var string The model name used for permission checking */
     protected string $modelName = 'role';
+
+    public function update(User $user, Model $model): bool
+    {
+        if (! $user->is_super_admin && $user->hasRole($model)) {
+            return false;
+        }
+
+        return parent::update($user, $model);
+    }
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,6 +2,9 @@
 
 namespace App\Policies;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+
 /**
  * User Policy
  *
@@ -16,4 +19,13 @@ class UserPolicy extends BasePolicy
 {
     /** @var string The model name used for permission checking */
     protected string $modelName = 'user';
+
+    public function update(User $user, Model $model): bool
+    {
+        if (! $user->is_super_admin && $user->id == $model->id) {
+            return false;
+        }
+
+        return parent::update($user, $model);
+    }
 }
